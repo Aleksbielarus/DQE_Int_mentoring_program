2a3fb49fef31f7c55016fcd2ea3ad2b19d60cbc1
Aliaksei Darashenka <57850783+Aleksbielarus@users.noreply.github.com>
2022-05-12 15:50:27 +0300

    add description

--- Module_4 Automation/PyTest_Task/tests/test_case.py
+++ Module_4 Automation/PyTest_Task/tests/test_case.py
@@ -6,6 +6,9 @@ from src.verifiecation_scripts import SQL_scripts
 
 
 def test_hr_empl_duplicates_by_key_checks():
+    """
+    Check uniqueness by PK in the table hr.employees
+    """
     cursor, conn = connection.mssql_connect(Credentials.server,
                                             Credentials.user,
                                             Credentials.password,
@@ -17,6 +20,10 @@ def test_hr_empl_duplicates_by_key_checks():
 
 
 def test_hr_empl_salary_validation():
+    """
+    Validate that there is no salary per employee more than max
+    salary for job possition or less than min_salary for job possition.
+    """
     cursor, conn = connection.mssql_connect(Credentials.server,
                                             Credentials.user,
                                             Credentials.password,
@@ -28,6 +35,9 @@ def test_hr_empl_salary_validation():
 
 
 def test_hr_empl_email_validity():
+    """
+     Validate that employees.email column contain @.
+    """
     cursor, conn = connection.mssql_connect(Credentials.server,
                                             Credentials.user,
                                             Credentials.password,
@@ -39,6 +49,9 @@ def test_hr_empl_email_validity():
 
 
 def test_hr_empl_manager_id_consistency():
+    """
+     Validate that employees.manager_id exists in employees.employee_id
+    """
     cursor, conn = connection.mssql_connect(Credentials.server,
                                             Credentials.user,
                                             Credentials.password,
@@ -50,6 +63,9 @@ def test_hr_empl_manager_id_consistency():
 
 
 def test_hr_location_postal_code_validity():
+    """
+     Validate that locations.postal_code consists of 5 symbols.
+    """
     cursor, conn = connection.mssql_connect(Credentials.server,
                                             Credentials.user,
                                             Credentials.password,
@@ -61,6 +77,9 @@ def test_hr_location_postal_code_validity():
 
 
 def test_hr_empl_hire_date_completeness():
+    """
+     Hire date column is comletely fill in  [hr].[employees] table
+    """
     cursor, conn = connection.mssql_connect(Credentials.server,
                                             Credentials.user,
                                             Credentials.password,
@@ -69,3 +88,4 @@ def test_hr_empl_hire_date_completeness():
     cursor.execute(SQL_scripts.hr_empl_hire_date_completeness_query())
     results = len(pd.DataFrame(cursor.fetchall()).index)
     assert results == 0, global_enums.WRONG_COUNT_OF_ROWS(results)
+
