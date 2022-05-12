b2f293608e5852a4c80f7fe34ae50d6df9fc44f1
Aliaksei Darashenka <57850783+Aleksbielarus@users.noreply.github.com>
2022-05-12 13:17:57 +0300

    Update test_case.py

--- Module_4 Automation/PyTest_Task/tests/test_case.py
+++ Module_4 Automation/PyTest_Task/tests/test_case.py
@@ -13,7 +13,7 @@ def test_hr_empl_duplicates_by_key_checks():
                                             Credentials.port)
     cursor.execute(SQL_scripts.hr_employee_duplicate_by_keys_query())
     results = len(pd.DataFrame(cursor.fetchall()).index)
-    assert results == 0
+    assert results == 0, global_enums.WRONG_COUNT_OF_ROWS(results)
 
 
 def test_hr_empl_salary_validation():
@@ -24,7 +24,7 @@ def test_hr_empl_salary_validation():
                                             Credentials.port)
     cursor.execute(SQL_scripts.hr_empl_salary_validation_query())
     results = len(pd.DataFrame(cursor.fetchall()).index)
-    assert results == 0
+    assert results == 0, global_enums.WRONG_COUNT_OF_ROWS(results)
 
 
 def test_hr_empl_email_validity():
@@ -35,7 +35,7 @@ def test_hr_empl_email_validity():
                                             Credentials.port)
     cursor.execute(SQL_scripts.hr_empl_email_validity_query())
     results = len(pd.DataFrame(cursor.fetchall()).index)
-    assert results == 0
+    assert results == 0, global_enums.WRONG_COUNT_OF_ROWS(results)
 
 
 def test_hr_empl_manager_id_consistency():
@@ -46,7 +46,7 @@ def test_hr_empl_manager_id_consistency():
                                             Credentials.port)
     cursor.execute(SQL_scripts.hr_empl_manager_id_consistancy_query())
     results = len(pd.DataFrame(cursor.fetchall()).index)
-    assert results == 0
+    assert results == 0, global_enums.WRONG_COUNT_OF_ROWS(results)
 
 
 def test_hr_location_postal_code_validity():
@@ -69,4 +69,3 @@ def test_hr_empl_hire_date_completeness():
     cursor.execute(SQL_scripts.hr_empl_hire_date_completeness_query())
     results = len(pd.DataFrame(cursor.fetchall()).index)
     assert results == 0, global_enums.WRONG_COUNT_OF_ROWS(results)
-
